ac68254b8c5585fa75649f129651f3069dbe4a9d
Nico <89244947+nicovalerian@users.noreply.github.com>
2024-05-31 02:06:31 +0700

    Add dropdown menu feature

--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,24 @@ songCards.forEach(card => {
         window.location.href = `/songDetail.html?id=${songId}`;
     });
 });
+
+// Dropdown menu fullscreen
+const dropdownMenu = document.querySelector('.dropdown-btn');
+
+if (dropdownMenu) {
+    dropdownMenu.addEventListener('click', () => {
+        const background = document.querySelector('.mega-menu');
+        
+        if (background) {
+            background.classList.toggle('mega-menu-active');
+            const otherContent = document.querySelector('.content');
+            otherContent.classList.toggle('display-none');
+
+            const exitButton = document.querySelector('.exit-btn');
+            exitButton.addEventListener('click', () => {
+                background.classList.remove('mega-menu-active');
+                otherContent.classList.remove('display-none');
+            });
+        }
+    });
+}
