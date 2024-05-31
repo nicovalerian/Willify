fe9dd64590ca7410b136024a0c2abbe45aac1c46
Nico <89244947+nicovalerian@users.noreply.github.com>
2024-05-31 23:09:56 +0700

    Add song detail data fetch

--- src/js/songDetail.js
+++ src/js/songDetail.js
@@ -1,7 +1,31 @@
-import { songs } from './songs.js'; // Import array songs dari databasee
+import { songs } from './database.js'; // Import array songs dari databasee
 
 // Tombol balik ke songs page
 const backButton = document.querySelector('.back-arrow-btn');
 backButton.addEventListener('click', () => {    
     window.location.href = '/songs.html';
 });
+
+// Ambil query parameter id dari URL
+const urlParams = new URLSearchParams(window.location.search);
+const songId = urlParams.get('id');
+const song = songs[songId];
+
+// Update konten song detail
+const albumCover = document.querySelector('.album-cover-img');
+const songTitle = document.querySelector('.song-title');
+const songArtist = document.querySelector('.artist-subtitle');
+const songAlbum = document.querySelector('.album-subtitle');
+const songDuration = document.querySelector('.song-duration');
+const songLyrics = document.querySelector('.lyrics-text');
+
+const songLyricsBr = song.lyrics.split('\n').join('<br>');
+
+if (song) {
+    albumCover.src = song.albumCoverPath;
+    songTitle.textContent = song.title;
+    songArtist.textContent = song.artist;
+    songAlbum.textContent = song.album;
+    songDuration.textContent = song.duration;
+    songLyrics.innerHTML = songLyricsBr;
+}
